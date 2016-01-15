ee037982765d93e53d5fddf4deaa021ab7ef8b2f
D. C. Rockwell <dcautomates@gmail.com>
2016-01-15 05:46:23 -0700

    onto the last 2 packager tests

--- es6/spec/packagers/nodejs/akiroPackager.spec.js
+++ es6/spec/packagers/nodejs/akiroPackager.spec.js
@@ -1,7 +1,13 @@
 import AkiroPackager from "../../../lib/akiro/packagers/nodejs/akiroPackager.js";
 import sinon from "sinon";
 import temp from "temp";
-import fileSystem from "fs";
+import fileSystem from "fs-extra";
+import { exec } from "child_process";
+import packageJson from "../../../../package.json";
+import unzip from "unzip2";
+import glob from "glob";
+import difference from "array-difference";
+import AWS from "aws-sdk";
 
 temp.track();
 
@@ -11,11 +17,18 @@ describe("AkiroPackager(event, context)", () => {
 			akiroPackager,
 			results,
 
+			nodeModulesDirectoryPath,
 			temporaryDirectoryPath,
-			mockNpmPath,
-			mockTemp;
 
-	beforeEach(done => {
+			mockExec,
+			mockNpmPath,
+			mockTemp,
+			mockAWS,
+			mockS3,
+
+			s3ConstructorSpy;
+
+	beforeEach((done) => {
 		temp.mkdir("akiroPackager", (error, newTemporaryDirectoryPath) => {
 			temporaryDirectoryPath = newTemporaryDirectoryPath;
 			done();
@@ -26,10 +39,38 @@ describe("AkiroPackager(event, context)", () => {
 	// 	temp.cleanup(done);
 	// });
 
-	beforeEach(done => {
-		event = {};
+	beforeEach(function (done) {
+		this.timeout(60000);
 
-		mockNpmPath = `${__dirname}/../../../../node_modules/npm`;
+		event = {
+			region: "us-east-1",
+			packages: {
+				"async": packageJson.dependencies.async
+			}
+		};
+
+		mockExec = sinon.spy((command, callback) => {
+			const nodeCliPath = `${temporaryDirectoryPath}/node_modules/npm/bin/npm-cli.js`;
+			switch(command) {
+				case `cd ${temporaryDirectoryPath};node ${nodeCliPath} init -y`:
+					const newPackageJson = require(`${__dirname}/../../fixtures/newPackage.json`);
+					fileSystem.writeFile(`${temporaryDirectoryPath}/package.json`, JSON.stringify(newPackageJson), (error) => {
+						callback(error);
+					});
+					break;
+				case `cd ${temporaryDirectoryPath};node ${nodeCliPath} install`:
+					fileSystem.copy(`${nodeModulesDirectoryPath}/async`, `${temporaryDirectoryPath}/node_modules/async`, (error) => {
+						callback(error);
+					});
+					break;
+				default:
+					callback(new Error(`exec called with '${command}', but it is not mocked.`));
+			}
+		});
+
+		nodeModulesDirectoryPath = `${__dirname}/../../../../node_modules`;
+
+		mockNpmPath = `${nodeModulesDirectoryPath}/npm`;
 
 		mockTemp = {
 			mkdir: sinon.spy((directoryName, callback) => {
@@ -37,12 +78,36 @@ describe("AkiroPackager(event, context)", () => {
 			})
 		};
 
+		s3ConstructorSpy = sinon.spy();
+
+		mockS3 = {
+			putObject: sinon.spy((parameters, callback) => {
+				callback();
+			})
+		};
+
+		class MockS3 {
+			constructor(config) {
+				s3ConstructorSpy(config);
+				return mockS3;
+			}
+		}
+
+		mockAWS = {
+			S3: MockS3
+		};
+
 		context = {
+			AWS: mockAWS,
+			exec: mockExec,
 			npmPath: mockNpmPath,
 			temp: mockTemp,
 			succeed: (succeedResults) => {
 				results = succeedResults;
 				done();
+			},
+			fail: (error) => {
+				done(error);
 			}
 		};
 
@@ -74,6 +139,30 @@ describe("AkiroPackager(event, context)", () => {
 		});
 	});
 
+	describe("akiroPackager.exec", () => {
+		it("should be set to context.exec if provided", () => {
+			akiroPackager.exec.should.eql(mockExec);
+		});
+
+		it("should be set to the exec package if not provided", () => {
+			context = {};
+			akiroPackager = new AkiroPackager(event, context);
+			akiroPackager.exec.should.eql(exec);
+		});
+	});
+
+	describe("akiroPackager.AWS", () => {
+		it("should be set to context.AWS if provided", () => {
+			akiroPackager.AWS.should.eql(MockAWS);
+		});
+
+		it("should be set to the AWS package if not provided", () => {
+			context = {};
+			akiroPackager = new AkiroPackager(event, context);
+			akiroPackager.AWS.should.eql(AWS);
+		});
+	});
+
 	describe("(Temporary Directory)", () => {
 		it("should create a temporary 'akiroPackager' directory", () => {
 			mockTemp.mkdir.firstCall.args[0].should.eql("akiroPackager");
@@ -100,14 +189,73 @@ describe("AkiroPackager(event, context)", () => {
 
 	it("should copy ./node_modules/npm to the temporary directory", () => {
 		const npmFileNames = fileSystem.readdirSync(`${temporaryDirectoryPath}/node_modules/npm`);
-		const expectedNpmFileNames = fileSystem.readdirSync(`${__dirname}/../../../../node_modules/npm`);
+		const expectedNpmFileNames = fileSystem.readdirSync(`${nodeModulesDirectoryPath}/npm`);
 
 		npmFileNames.should.have.members(expectedNpmFileNames);
 	});
 
-	it("should create an empty package.json to satisfy npm");
-	it("should use npm to install and build all designated packages");
-	it("should add all designated package code to a .zip file");
-	it("should copy the .zip file to the designated S3 bucket");
-	it("should copy the .zip file to the designated S3 key");
+	it("should create an empty package.json to satisfy npm", () => {
+		fileSystem.existsSync(`${temporaryDirectoryPath}/package.json`).should.be.true;
+	});
+
+	it("should add the designated packages as dependencies in the new package.json", () => {
+		const newPackageJson = require(`${temporaryDirectoryPath}/package.json`);
+		newPackageJson.dependencies.should.eql(event.packages);
+	});
+
+	it("should use npm to install and build all designated packages", () => {
+		const asyncFileNames = fileSystem.readdirSync(`${temporaryDirectoryPath}/node_modules/async`);
+		const expectedAsyncFileNames = fileSystem.readdirSync(`${nodeModulesDirectoryPath}/async`);
+
+		asyncFileNames.should.have.members(expectedAsyncFileNames);
+	});
+
+	it("should add all designated package code to a .zip file", function (done) {
+		/* eslint-disable new-cap */
+		this.timeout(60000);
+
+		const zipFilePath = `${temporaryDirectoryPath}/packages.zip`;
+
+		let asyncFilePaths = glob.sync(`${temporaryDirectoryPath}/node_modules/async/**/*`, { dot: true });
+		let npmFilePaths = glob.sync(`${temporaryDirectoryPath}/node_modules/npm/**/*`, { dot: true });
+
+		asyncFilePaths = asyncFilePaths.filter(filterDirectories);
+		npmFilePaths = npmFilePaths.filter(filterDirectories);
+
+		function filterDirectories(filePath) {
+			return !fileSystem.statSync(filePath).isDirectory();
+		}
+
+		let expectedFilePaths = asyncFilePaths.concat(npmFilePaths);
+		expectedFilePaths = expectedFilePaths.map((filePath) => {
+			const relativePath = filePath.replace(`${temporaryDirectoryPath}/node_modules/`, "");
+			return relativePath;
+		});
+
+		let filePaths = [];
+		fileSystem.createReadStream(zipFilePath)
+			.pipe(unzip.Parse())
+			.on("entry", (entry) => {
+				filePaths.push(entry.path);
+			})
+			.on("close", () => {
+				difference(expectedFilePaths, filePaths).should.eql([]);
+				done();
+			});
+	});
+
+	it("should instantiate S3 with the designated region", () => {
+		s3ConstructorSpy.calledWith({
+			region: event.region
+		});
+	});
+
+	it("should copy the .zip file to the designated S3 options", () => {
+		const putObjectParameters = {
+			Bucket: "",
+			Key: "",
+			Body: ""
+		};
+		mockS3.putObject.calledWith().should.be.true;
+	});
 });
